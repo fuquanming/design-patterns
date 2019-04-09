7d0aaa357d406fb730112c5d5ecd5deb4b611ac4
fqm <67837343@qq.com>
2019-04-09 22:19:08 +0800

    总结

---
+++ summary/src/main/java/com/demo/spring/aop/SpringAopApplication.java
@@ -0,0 +1,20 @@
+package com.demo.spring.aop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author: admin
+ * @create: 2019/3/25
+ * @update: 11:21
+ * @version: V1.0
+ * @detail:
+ **/
+@SpringBootApplication
+public class SpringAopApplication {
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAopApplication.class,args);
+    }
+}
---
+++ summary/src/main/java/com/demo/spring/aop/mode/Member.java
@@ -0,0 +1,11 @@
+package com.demo.spring.aop.model;
+
+/**
+ * @author: admin
+ * @create: 2019/3/25
+ * @update: 10:15
+ * @version: V1.0
+ * @detail:
+ **/
+public class Member {
+}
